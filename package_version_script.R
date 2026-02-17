6774f5af9f173a28a20049c0eb7abf003b500fda
Kaitlyn Schisler <162852955+k-schisler@users.noreply.github.com>
2026-02-17 15:31:18 -0600

    Updates on Visualizations and Tables; New script.
    Created a script for checking the version number of the in use packages
    for citations.
    
    Visualizations qmd updated to use language "classifiers" rather than
    categories when referring to the CRC progression as it seemed more
    appropriate to remain consistent. Completed smoking and alcohol
    analysis, including transformations on those who show drinking (values
    >0) to handle skewedness. Other small formatting changes.
    
    Tables qmd changes from using fave stats to using dplyr to have more
    control over what is displayed and how it is presented. Found a flow to
    ensure that all tables generated follow the same general formatting for
    standardization. Age and Gender completed. Save future work for when we
    narrow scope on what topics we will focus on for our assignment
    submission and focus on those tables next.
    
    Next tasks:
    - Any other tables needed on the data we plan to present in our EDA
      assignment submission
    - Statistical tests to support or fail to support observed differences
      in the tables/visuals
    - Download qualifying studys and observe the taxonomic/metabolic data,
      perform similar EDA on this data

---
+++ package_version_script.R
@@ -0,0 +1,13 @@
+# Package Versions Loaded/Installed
+###################################
+
+cat(
+  "Bioconductor:", as.character(packageVersion("BiocManager")),
+  "\ndplyr:", as.character(packageVersion("dplyr")),
+  "\nggplot2:", as.character(packageVersion("ggplot2")),
+  "\npatchwork:", as.character(packageVersion("patchwork")),
+  "\nknitr:", as.character(packageVersion("knitr")),
+  "\nstringr:", as.character(packageVersion("stringr")),
+  "\nkableExtra:", as.character(packageVersion("kableExtra")),
+  "\nmosaic:", as.character(packageVersion("mosaic"))
+  )
