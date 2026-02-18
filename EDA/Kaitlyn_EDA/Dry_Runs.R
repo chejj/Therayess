97919b0108b221c413d1e1a84db56aa52694f3f9
Kaitlyn Schisler <162852955+k-schisler@users.noreply.github.com>
2026-02-18 17:31:16 -0600

    Run Script Updates
    Dry runs script updates to only contain info availability as well as dry
    run only. All code for test and wet run moved to respectively named
    script files.
    
    New wet run script file.
    
    Test run script file changed to have Thomas 2018a as it contains 80
    samples with almost all CRC progression classifiers represented,
    resulting in a better overall representation of the sample in a small
    download for testing on personal machine.

--- EDA/Kaitlyn_EDA/Dry_Runs.R
+++ EDA/Kaitlyn_EDA/Dry_Runs.R
@@ -18,17 +18,11 @@ for (study in studies) {
   writeLines(curatedMetagenomicData(study, dryrun = TRUE), outfile)
 }
 
-# Option for Wet Run
-####################
-# Interested in:
-focus <- c(
-  "relative_abundance", # taxa
-  "pathway_abundance",  # pathway abundance (metabolic)
-  "pathway_coverage")   # pathway "presence" (metabolic)
+# Dry Run
+#########
 
-# "For each data type I care about, build a search pattern that matches all my studies, download only that data type, and merge all studies into one combined dataset."
-for (type in focus) {
-  pattern <- paste0("(", paste(studies, collapse = "|"), ").*", type, "$") # Studies, separated by "OR" ("|"), then ".*" [type] and the "$" meaning END LINE
-  x_list <- curatedMetagenomicData(pattern, dryrun = TRUE) # Change this to FALSE when time comes
-  merged_by_focus[[type]] <- mergeData(x_list) # Merges thesummarizedTreeExperiments into one, by focus
-}
+Test <- curatedMetagenomicData("ThomasAM_2018b.+", 
+                               dryrun = TRUE) # Doesn't download the data, dry runs it
+class(Test)
+length(Test)
+head(Test)
--- EDA/Kaitlyn_EDA/Test_Run.R
+++ EDA/Kaitlyn_EDA/Test_Run.R
@@ -1,15 +1,12 @@
 ##############################################################
-# Testing with Single Small Study (ThomasAM_2018b, 59 Samples)
+# Testing with Single Small Study (ThomasAM_2018a, 80 Samples)
 ##############################################################
+# Check Thomas2018a as it is small 
+# w/ most categories (HC, PA, PA+, CRC, CRC+, Other)
+# ----
+
 # Run Qualifying Studies List
 source("./EDA/Qualifying_Studies_List.R")
-
-
-Test <- curatedMetagenomicData("ThomasAM_2018b.+", 
-                               dryrun = TRUE) # Doesn't download the data, dry runs it
-class(Test)
-length(Test)
-head(Test)
 
 # Interested in:
 ################
@@ -18,23 +15,22 @@ focus <- c(
   "pathway_abundance",  # pathway abundance (metabolic)
   "pathway_coverage")   # pathway "presence" (metabolic)
 
-
 #############################
 # Exploring Taxa and Pathways
 #############################
-ThomasAM2018b_list <- list()
+ThomasAM2018a_list <- list() 
 
 for (type in focus) {   # Loop through what we want to focus on
-  pattern <- paste0("ThomasAM_2018b.", type, "$") # What we are looking for in the "information available per study" generated above in .txt files
+  pattern <- paste0("ThomasAM_2018a.", type, "$") # What we are looking for in the "information available per study" generated above in .txt files
                                                   # "$" means "match this exactly"
-  ThomasAM2018b_list[[type]] <- curatedMetagenomicData(  # create new list element by the "type" currently being looped through
+  ThomasAM2018a_list[[type]] <- curatedMetagenomicData(  # create new list element by the "type" currently being looped through
     pattern,
     dryrun = FALSE
   )[[1]]
   
 }
 
-class(ThomasAM2018b_list)
-length(ThomasAM2018b_list)
-head(ThomasAM2018b_list)
+class(ThomasAM2018a_list)
+length(ThomasAM2018a_list)
+head(ThomasAM2018a_list)
 
