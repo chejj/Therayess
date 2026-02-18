af587d7bd120d13ecd1dcd554b1a16890e55bd43
Kaitlyn Schisler <162852955+k-schisler@users.noreply.github.com>
2026-02-17 21:12:06 -0600

    Testing Microbiome Download; Fix Source Script Runs
    Making test runs on local computer with Thomas AM 2018b to see how large
    the file is as well as what types of data is downloaded.
    
    Other misc bug fixes in other files.
    
    I've attempted attaching Git to Sol, while it appears to attach, it
    requires passwork rather than the PAT which seems odd. May need to
    trouble shoot this. Perhaps an SSH key instead?

--- EDA/Kaitlyn_EDA/Dry_Runs.R
+++ EDA/Kaitlyn_EDA/Dry_Runs.R
@@ -16,3 +16,17 @@ for (study in studies) {
   outfile <- paste0("./EDA/Kaitlyn_EDA/Info_Availability/", study, "_info_availability.txt")
   writeLines(curatedMetagenomicData(study, dryrun = TRUE), outfile)
 }
+
+##############################################################
+# Testing with Single Small Study (ThomasAM_2018b, 59 Samples)
+##############################################################
+Test <- curatedMetagenomicData("ThomasAM_2018b.+", 
+                               dryrun = TRUE) # Doesn't download the data, dry runs it
+class(Test)
+length(Test)
+head(Test)
+
+# Exploring Taxa Abundance
+##########################
+ThomasAM2018b_taxa_test <- curatedMetagenomicData("ThomasAM_2018b.+", 
+                                                  dryrun = FALSE) # Actually downloads the data
